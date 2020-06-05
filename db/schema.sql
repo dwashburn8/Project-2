6bf27f905897e6d13f53dd0c12de9621332c7cb4
Dallas Washburn <soldbydw@gmail.com>
2020-06-04 21:22:30 -0700

    started html

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,17 @@
-DROP DATABASE IF EXISTS projecttwo_db;
-CREATE DATABASE projecttwo_db;
+DROP DATABASE IF EXISTS houses_db;
+CREATE DATABASE houses_db;
+
+USE houses_db;
+
+CREATE TABLE house(
+id INT AUTO_INCREMENT NOT NULL,
+city VARCHAR(1000),
+address VARCHAR(1000),
+price INT,
+square_feet INT,
+bedrooms INT,
+bathrooms INT,
+pool BOOLEAN,
+
+Primary Key(id)
+);
